24f1513ab70259936e1fd391c42a2bb544ca0f08
Elise Rosky <emrosky@mtu.edu>
2023-10-10 10:17:25 -0400

    NCAR Reconstruction data

---
+++ Updraft_Analysis/SPICULE/Entrainment_Analysis/NCAR_reconstruction/core_edge_cdf_comparison_curves.m
@@ -0,0 +1,38 @@
+function out = core_edge_comparison_curves(region)
+
+% get the core-edge pairs from core_edge_pairs.csv for each pair, 
+% get all the comparison data output a table containing the data 
+% CorePassNum, EdgePassNum, Core90th, Core_Avg_Large, Core_Largest,
+% Edge90th, Edge_Avg_Large, Edge_Largest,
+
+% Plot a three panel figure per pair in the Region folder
+
+region_folder = '../../';
+timestamps = readtable(fullfile(region_folder, region, 'core_edge_pairs.csv'));
+
+edge = readtable(fullfile(region_folder, region, 'EdgeCloud', 'timestamps.csv'));
+core = readtable(fullfile(region_folder, region, 'InCloud', 'timestamps.csv'))
+
+output_path = '../';
+output_folder = fullfile(output_path, region, 'NCAR_reconstruction');
+
+
+for r=1 : height(timestamps)
+   % get droplet size data
+   core_pass = timestamps{r,2}
+   edge_pass = timestamps{r,1}
+   
+   corestart = core.StartTime(core_pass)
+   coreend = core.EndTime(core_pass)
+   
+   edgestart = edge.StartTime(edge_pass)
+   edgeend = edge.EndTime(edge_pass)
+   
+   titlestring = region + "Edge" + edge_pass + "_Core" + core_pass;
+   plot_two_cdfs_continuous(region, corestart, coreend, edgestart, edgeend, titlestring, output_folder);
+   
+   
+end
+
+
+end
---
+++ Updraft_Analysis/SPICULE/Entrainment_Analysis/NCAR_reconstruction/plot_two_cdfs_continuous.m
@@ -0,0 +1,56 @@
+function [] = plot_two_cdfs_continuous(region, corestart, coreend, edgestart, edgeend, titlestring, outputfolder);
+
+% high resoltion time series of holodec
+holo_path = sprintf('../../%s/NCAR_reconstruction', region);
+holofile = dir(fullfile(holo_path, '*_HOLODEC.nc'));
+holo_nc = fullfile(holo_path, holofile.name);
+
+holotimes = ncread(holo_nc,'particletime');
+diameters = ncread(holo_nc,'d');
+totalN = length(diameters);
+
+date_ref = split(holofile.name, "_")
+year = date_ref{2}(1:4)
+month = date_ref{2}(5:6)
+day = date_ref{2}(7:8)
+
+OFFSET = -seconds(1.0);
+holotimes = datetime(str2double(year),str2double(month),str2double(day)) + seconds(holotimes(:,1)) + OFFSET;
+coreIndexes = (holotimes >= corestart) & (holotimes <= coreend);
+edgeIndexes = (holotimes >= edgestart) & (holotimes <= edgeend);
+
+coreDiam = diameters(coreIndexes);
+edgeDiam = diameters(edgeIndexes);
+
+% Find total sample volume of all holograms combined
+dy = 1.0; %cm ***NEEDS TO BE CHECKED
+dx = 1.44; %cm
+dz = 13; %cm
+sample_volume = dy*dx*dz; %cubic cm
+
+
+% diameters1 = quicklook1.ans.eqDiam*1000000;
+% diameters2 = quicklook2.ans.eqDiam*1000000;
+
+C_query = 0:0.01:1.0;
+
+[C1, d1, Clo1, Cup1]  = ecdf(coreDiam,'Bounds','on');
+[C2, d2, Clo2, Cup2]  = ecdf(edgeDiam,'Bounds','on');
+
+core_interp = interp1(C1, d1, C_query);
+edge_interp = interp1(C2, d2, C_query);
+
+%Plot droplet size distribution in #/cc/um
+fig = figure('visible','off');
+ecdf(coreDiam,'Bounds','on'); hold on
+%plot(d1, C1, 'LineWidth', 2, 'DisplayName','Core')
+hold on
+ecdf(edgeDiam,'Bounds','on'); hold on
+%plot(d2, C2, 'LineWidth', 2, 'DisplayName','Edge')
+hold on
+
+xlabel('Diameter (microns)'), ylabel('Mass CDF')
+legend('Core','Lower Confidence Bound','Upper Confidence Bound','Edge','Location','best')
+title(titlestring,  'interpreter', 'none')
+
+saveas(fig, sprintf('%s/%s_cdf_NCAR.png', outputfolder, titlestring));
