b277f190f378475558ef09070c34d7146d081804
Elise Rosky <emrosky@mtu.edu>
2023-10-10 19:00:35 -0400

    ncar dsd's

---
+++ Updraft_Analysis/SPICULE/Entrainment_Analysis/NCAR_reconstruction/plot_two_pdfs.m
@@ -0,0 +1,22 @@
+function [] = plot_two_pdfs(region, corestart, coreend, edgestart, edgeend, numberofbins, titlestring, outputfolder);
+
+[Dcenters1, C1, err1] = pdf_test(region, corestart, coreend, numberofbins)
+[Dcenters2, C2, err2] = pdf_test(region, edgestart, edgeend, numberofbins)
+
+
+%Plot droplet size distribution in #/cc/um
+fig = figure('visible','off');
+plot(Dcenters1.*1000000, C1, 'LineWidth', 2, 'DisplayName','Core')
+hold on
+er = errorbar(Dcenters1.*1000000, C1, err1, "o", 'LineWidth', 1, 'Color', 'b',  'HandleVisibility','off');
+hold on
+plot(Dcenters2.*1000000, C2, 'LineWidth', 2, 'DisplayName','Edge')
+hold on
+er = errorbar(Dcenters2.*1000000, C2, err2, "o", 'LineWidth', 1, 'Color', '#EDB120',  'HandleVisibility','off');
+hold on
+set(gca,'YScale','log','XScale','log')
+xlabel('Diameter (microns)'), ylabel('Concentration (#/cc)')
+legend
+title(titlestring,  'interpreter', 'none')
+
+saveas(fig, sprintf('%s/%s_curves.png', outputfolder, titlestring));
